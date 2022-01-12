398d706bb617d5d022e6fd65e8743aa614e746cb
unknown <nirupama1210@gmail.com>
2022-01-12 11:40:43 +0530

    PROGRAD 5622

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
 
+
+function data(name, year, qualification){
+    userData = {
+        name: name,
+        Year: year,
+        Qualification: qualification,
+    }
+    return userData
+}
+
+module.exports = data
